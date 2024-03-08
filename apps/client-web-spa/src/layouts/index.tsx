917c83b6b8bc04799b0bae62c6e4d732e8c55efd
tcdw <tcdw2011@gmail.com>
2024-03-08 22:21:07 +0800

    初始化

--- apps/client-web-spa/src/layouts/index.tsx
+++ apps/client-web-spa/src/layouts/index.tsx
@@ -1,44 +1,10 @@
 import { Outlet } from 'umi';
-import {Box} from "@chakra-ui/react";
 import React from "react";
-import {shallow} from "zustand/shallow";
-import YuruChara from "@/components/YuruChara";
-import DownloadManager from "@/components/DownloadManager";
-import AudioPlayer from "@/components/AudioPlayer";
-import Footer from "@/components/Footer";
-import SearchBox from "@/components/SearchBox";
-import Header from "@/components/Header";
-import {useBilisoundStore} from "@/store/bilisoundStore";
-import {BOTTOM_HEIGHT} from "@/constants";
 
-const App = () => {
-    // 内容
-    const {
-        playingEpisode,
-    } = useBilisoundStore((state) => ({
-        playingEpisode: state.playingEpisode,
-    }), shallow);
-
-    const withoutVideo = playingEpisode <= 0;
-
+export default function Layout() {
     return (
-        <>
-            <Box display="flex" flexDirection="column" minH={withoutVideo ? "100dvh" : `calc(100dvh - ${BOTTOM_HEIGHT})`}>
-                <Header />
-                <Box
-                    flexGrow={1}
-                    position="relative"
-                >
-                    <SearchBox />
-                    <Outlet />
-                </Box>
-                <Footer />
-            </Box>
-            {withoutVideo ? "" : <AudioPlayer />}
-            <DownloadManager />
-            <YuruChara />
-        </>
+        <div>
+            <Outlet />
+        </div>
     );
-};
-
-export default App;
+}

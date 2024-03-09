eab741a3edf2b236d0c263e4b8e3205897149345
tcdw <tcdw2011@gmail.com>
2024-03-09 19:57:12 +0800

    看板娘

--- apps/client-web-spa/src/components/YuruChara.tsx
+++ apps/client-web-spa/src/components/YuruChara.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import backgroundCorner from "@/assets/bg-corner.svg";
-import { Box, useColorModeValue } from "@chakra-ui/react";
+import { css } from "@/styled-system/css";
 
 const YuruChara: React.FC = () => (
-    <Box
+    <div
         aria-hidden
-        w="250px"
-        h="250px"
-        position="fixed"
-        right="0"
-        bottom="150px"
-        background={`url(${backgroundCorner})`}
-        zIndex={1}
-        opacity={0.3}
-        pointerEvents="none"
-        mixBlendMode={useColorModeValue("multiply", "lighten")}
+        style={{
+            background: `url(${backgroundCorner})`,
+        }}
+        className={css({
+            w: "250px",
+            h: "250px",
+            position: "fixed",
+            right: 0,
+            bottom: "150px",
+            zIndex: 1,
+            opacity: 0.3,
+            pointerEvents: "none",
+            mixBlendMode: {
+                base: "multiply",
+                _dark: "lighten",
+            },
+        })}
     />
 );
 export default YuruChara;
--- apps/client-web-spa/src/layouts/index.tsx
+++ apps/client-web-spa/src/layouts/index.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "umi";
 import React from "react";
 import Header from "@/layouts/components/Header";
+import YuruChara from "@/components/YuruChara";
 
 export default function Layout() {
     return (
         <div>
             <Header />
             <Outlet />
+            <YuruChara />
         </div>
     );
 }

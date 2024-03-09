b860640482477cdee482fceb142de00b75feb321
tcdw <tcdw2011@gmail.com>
2024-03-09 18:52:47 +0800

    完善暗色模式

--- apps/client-web-spa/src/layouts/components/ColorModeButton.tsx
+++ apps/client-web-spa/src/layouts/components/ColorModeButton.tsx
@@ -3,8 +3,9 @@ import * as Popover from "@radix-ui/react-popover";
 import { center, flex } from "@/styled-system/patterns";
 import { css } from "@/styled-system/css";
 import { ReactComponent as IconAuto } from "@/icons/ic--round-auto-awesome.svg";
-import { ReactComponent as IconDark } from "@/icons/fa-solid--sun.svg";
-import { ReactComponent as IconLight } from "@/icons/fa-solid--moon.svg";
+import { ReactComponent as IconDark } from "@/icons/fa-solid--moon.svg";
+import { ReactComponent as IconLight } from "@/icons/fa-solid--sun.svg";
+import useColorModeStore from "@/store/colorMode";
 
 const menuItem = flex({
     w: "full",
@@ -30,6 +31,11 @@ const menuItem = flex({
 });
 
 export default function ColorModeButton() {
+    const { actualColorMode, setColorMode } = useColorModeStore(state => ({
+        actualColorMode: state.actualColorMode,
+        setColorMode: state.setColorMode,
+    }));
+
     return (
         <Popover.Root>
             <Popover.Trigger asChild>
@@ -51,18 +57,11 @@ export default function ColorModeButton() {
                         },
                     })}
                 >
-                    <svg
-                        aria-hidden="true"
-                        className={css({ w: 5, h: 5 })}
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 384 512"
-                    >
-                        <path
-                            fill="currentColor"
-                            d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"
-                        ></path>
-                    </svg>
+                    {actualColorMode === "dark" ? (
+                        <IconDark className={css({ w: 5, h: 5 })} />
+                    ) : (
+                        <IconLight className={css({ w: 5, h: 5 })} />
+                    )}
                 </button>
             </Popover.Trigger>
             <Popover.Portal>
@@ -88,15 +87,15 @@ export default function ColorModeButton() {
                     })}
                     sideOffset={5}
                 >
-                    <button type={"button"} className={menuItem}>
+                    <button type={"button"} className={menuItem} onClick={() => setColorMode("light")}>
                         <IconLight className={css({ color: "primary.500" })} />
                         明亮
                     </button>
-                    <button type={"button"} className={menuItem}>
+                    <button type={"button"} className={menuItem} onClick={() => setColorMode("dark")}>
                         <IconDark className={css({ color: "primary.500" })} />
                         暗黑
                     </button>
-                    <button type={"button"} className={menuItem}>
+                    <button type={"button"} className={menuItem} onClick={() => setColorMode("system")}>
                         <IconAuto className={css({ color: "primary.500" })} />
                         跟随系统
                     </button>
---
+++ apps/client-web-spa/src/store/colorMode.ts
@@ -0,0 +1,75 @@
+// MediaQueryList
+import { create } from "zustand";
+
+function normalizedDict() {
+    if (localStorage.theme !== "dark" && localStorage.theme !== "light" && localStorage.theme !== "system") {
+        localStorage.theme = "system";
+    }
+    return localStorage.theme;
+}
+
+export type ActualColorMode = "dark" | "light";
+export type ColorMode = "dark" | "light" | "system";
+
+export interface ColorModeStoreProps {
+    colorMode: ColorMode;
+    actualColorMode: ActualColorMode;
+}
+
+export interface ColorModeStoreMethods {
+    setColorMode: (colorMode: ColorMode) => void;
+    setActualColorMode: (actualColorMode: ActualColorMode) => void;
+}
+
+const useColorModeStore = create<ColorModeStoreProps & ColorModeStoreMethods>()(setState => ({
+    colorMode: normalizedDict(),
+    setColorMode: colorMode => {
+        switch (colorMode) {
+            case "dark":
+                document.documentElement.classList.add("dark");
+                setState(() => ({ actualColorMode: "dark" }));
+                break;
+            case "light":
+                document.documentElement.classList.remove("dark");
+                setState(() => ({ actualColorMode: "light" }));
+                break;
+            default:
+                if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+                    document.documentElement.classList.add("dark");
+                    setState(() => ({ actualColorMode: "dark" }));
+                } else {
+                    document.documentElement.classList.remove("dark");
+                    setState(() => ({ actualColorMode: "light" }));
+                }
+                break;
+        }
+        localStorage.theme = colorMode;
+        setState(() => ({ colorMode }));
+    },
+    actualColorMode: window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light",
+    setActualColorMode: actualColorMode => {
+        setState(() => ({ actualColorMode }));
+    },
+}));
+
+const activateDarkMode = (dark: boolean) => {
+    const state = useColorModeStore.getState();
+    if (state.colorMode !== "system") {
+        return;
+    }
+    if (dark) {
+        document.documentElement.classList.add("dark");
+        state.setActualColorMode("dark");
+    } else {
+        document.documentElement.classList.remove("dark");
+        state.setActualColorMode("light");
+    }
+};
+
+const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+
+darkModePreference.addEventListener("change", e => {
+    activateDarkMode(e.matches);
+});
+
+export default useColorModeStore;

2f838e5c016cafcff9e1cc64fee3b668fecce86b
tcdw <tcdw2011@gmail.com>
2024-03-22 18:56:26 +0800

    系统设置页面

--- apps/client-web-spa/src/components/YuruChara.tsx
+++ apps/client-web-spa/src/components/YuruChara.tsx
@@ -1,30 +1,40 @@
 import React from "react";
 import backgroundCorner from "@/assets/bg-corner.svg";
 import { css } from "@/styled-system/css";
+import { useConfigStore } from "@/store/config";
 
-const YuruChara: React.FC = () => (
-    <div
-        aria-hidden
-        style={{
-            background: `url(${backgroundCorner})`,
-        }}
-        className={css({
-            w: "250px",
-            h: "250px",
-            position: "fixed",
-            right: 0,
-            bottom: "150px",
-            zIndex: 1,
-            opacity: {
-                base: 0.3,
-                _dark: 0.2,
-            },
-            pointerEvents: "none",
-            mixBlendMode: {
-                base: "multiply",
-                _dark: "lighten",
-            },
-        })}
-    />
-);
-export default YuruChara;
+export default function YuruChara() {
+    const { showYuruChara } = useConfigStore(state => ({
+        showYuruChara: state.showYuruChara,
+    }));
+
+    if (!showYuruChara) {
+        return null;
+    }
+
+    return (
+        <div
+            aria-hidden
+            style={{
+                background: `url(${backgroundCorner})`,
+            }}
+            className={css({
+                w: "250px",
+                h: "250px",
+                position: "fixed",
+                right: 0,
+                bottom: "150px",
+                zIndex: 1,
+                opacity: {
+                    base: 0.3,
+                    _dark: 0.2,
+                },
+                pointerEvents: "none",
+                mixBlendMode: {
+                    base: "multiply",
+                    _dark: "lighten",
+                },
+            })}
+        />
+    );
+}

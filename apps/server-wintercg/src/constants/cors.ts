dfc93a71a8f46ee2bac50313b9a91b1f7d1e2d3f
tcdw <tcdw2011@gmail.com>
2024-07-21 21:39:29 +0800

    修改常量

--- apps/server-wintercg/src/constants/cors.ts
+++ apps/server-wintercg/src/constants/cors.ts
@@ -1,5 +1,5 @@
 const CORS_HEADERS = {
-    "Access-Control-Allow-Origin": "https://bilisound.moe",
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
     "Access-Control-Max-Age": "86400",
     "Access-Control-Allow-Headers": "content-type",

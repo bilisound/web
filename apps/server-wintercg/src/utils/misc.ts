5bebfeff47c680a9dfa53896c21a74342ff484f3
tcdw <tcdw2011@gmail.com>
2024-07-22 14:15:31 +0800

    移除了没有用的 lodash

--- apps/server-wintercg/src/utils/misc.ts
+++ apps/server-wintercg/src/utils/misc.ts
@@ -1,42 +1,34 @@
-import { merge } from "lodash-es";
 import CORS_HEADERS from "../constants/cors";
-import { WebPlayInfo } from "../types";
 
-export const ajaxSuccess = (data: unknown, options: RequestInit = {}) => {
+export const ajaxSuccess = (data: unknown) => {
     return new Response(
         JSON.stringify({
             data,
             code: 200,
             msg: "ok",
         }),
-        merge(
-            {
-                headers: {
-                    ...CORS_HEADERS,
-                    "Content-Type": "application/json",
-                },
-            } as ResponseInit,
-            options,
-        ),
+        {
+            headers: {
+                ...CORS_HEADERS,
+                "Content-Type": "application/json",
+            },
+        },
     );
 };
 
-export const ajaxError = (msg: unknown, code = 500, options: RequestInit = {}) => {
+export const ajaxError = (msg: unknown, code = 500) => {
     return new Response(
         JSON.stringify({
             code,
             data: null,
             msg: String(msg),
         }),
-        merge(
-            {
-                status: code,
-                headers: {
-                    ...CORS_HEADERS,
-                    "Content-Type": "application/json",
-                },
-            } as ResponseInit,
-            options,
-        ),
+        {
+            status: code,
+            headers: {
+                ...CORS_HEADERS,
+                "Content-Type": "application/json",
+            },
+        },
     );
 };
